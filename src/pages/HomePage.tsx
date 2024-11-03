85799a4ecf5e833be6f5a976f4240f04f2b964a6
rbtavares <rafael@rbtavares.com>
2024-11-03 11:18:53 +0000

    readjusted sizing and set new fonts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,12 +8,12 @@ const breadcrumbRoutes = [
 
 const HomePage = () => {
   return (
-    <div className="bg-zinc-100 h-screen w-full flex flex-col items-center">
+    <div className="bg-zinc-100 h-screen w-full flex flex-col items-center bg-macos3 bg-center bg-cover">
       <Navbar routes={breadcrumbRoutes} />
       <div className="h-full w-full max-w-screen-2xl grid grid-cols-4 gap-5 p-5">
 
         <div className="flex flex-col gap-5">
-          <div className="card w-full h-1/3 min-h-48 max-h-60"><ProfileCard /></div>
+          <div className="card w-full h-52"><ProfileCard /></div>
           <div className="card w-full flex-1"></div>
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {},
-  		animation: {
-  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
-  			shine: 'shine var(--duration) infinite linear'
-  		},
-  		keyframes: {
-  			'border-beam': {
-  				'100%': {
-  					'offset-distance': '100%'
-  				}
-  			},
-  			shine: {
-  				'0%': {
-  					'background-position': '0% 0%'
-  				},
-  				'50%': {
-  					'background-position': '100% 100%'
-  				},
-  				to: {
-  					'background-position': '0% 0%'
-  				}
-  			}
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		fontFamily: {
+			'sans': ['Inter', 'sans-serif'],
+			'serif': ['Roboto Serif', 'serif'],
+			'mono': ['Roboto Mono', 'monos']
+		},
+		extend: {
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			colors: {},
+			animation: {
+				'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
+				shine: 'shine var(--duration) infinite linear'
+			},
+			backgroundImage: {
+				'macos': "url('/images/macos.jpg')",
+				'macos2': "url('/images/macos2.jpg')",
+				'macos3': "url('/images/macos3.jpg')",
+			},
+			keyframes: {
+				'border-beam': {
+					'100%': {
+						'offset-distance': '100%'
+					}
+				},
+				shine: {
+					'0%': {
+						'background-position': '0% 0%'
+					},
+					'50%': {
+						'background-position': '100% 100%'
+					},
+					to: {
+						'background-position': '0% 0%'
+					}
+				}
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 }
