cec194515ddad7768e2848fb1122a8678972f22f
rbtavares <73722899+rbtavares@users.noreply.github.com>
2024-11-05 10:20:30 +0000

    profile card now responsive

--- src/cards/ProfileCard.tsx
+++ src/cards/ProfileCard.tsx
@@ -7,25 +7,27 @@ const ProfileCard = () => {
   const formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 
   return (
-    <div className='flex w-full h-52 p-4 gap-4 card text-white'>
+    <div className='flex w-full h-36 2xl:h-52 p-3 2xl:p-4 gap-4 card text-white'>
 
       {/* Image & Identifiers */}
-      <div className='h-full flex flex-col items-center justify-between'>
-        <img className='drop-shadow-md aspect-square w-28 rounded-md flex items-center justify-center object-cover object-center' src={Avatar} />
+      <div className='h-full flex flex-col items-center justify-between gap-1'>
+        <img className='drop-shadow-md aspect-square w-20 2xl:w-28 rounded-md flex items-center justify-center object-cover object-center' src={Avatar} />
         <div className='flex flex-col items-center flex-1  w-full justify-center'>
-          <span className='drop-shadow-md'>t.scott</span>
-          <span className='drop-shadow-md font-mono text-xs'>69420</span>
+          <span className='drop-shadow-md 2xl:text-lg'>t.scott</span>
+          <span className='drop-shadow-md font-mono text-xs 2xl:text-sm'>69420</span>
         </div>
       </div>
 
       {/* Name & Greeting */}
       <div className='flex flex-col flex-1 h-full gap-2 justify-between'>
-          <h3 className='drop-shadow-md'>Good {date.getHours() > 5 ? date.getHours() > 12 ? <>afternoon</> : <>morning</>: <>evening</>},</h3>
-          <h1 className='drop-shadow-md text-5xl font-medium tracking-tight'>Mr Travis Scott</h1>
-          <h3 className='drop-shadow-md text-center opacity-50 italic text-sm'>{formattedDate}</h3>
+        <div className='flex flex-col gap-1'>
+          <h3 className='drop-shadow-md text-sm 2xl:text-base'>Good {date.getHours() > 5 ? date.getHours() > 12 ? <>afternoon</> : <>morning</> : <>evening</>},</h3>
+          <h1 className='drop-shadow-md text-2xl lg:text-3xl 2xl:text-5xl font-medium'>Frederico Silva</h1>
+        </div>
+        <h3 className='drop-shadow-md text-center opacity-50 italic text-xs 2xl:text-sm'>{formattedDate}</h3>
       </div>
 
-      
+
       <BorderBeam colorFrom="#ffffff" colorTo='#ffffff00' size={250} duration={12} delay={9} />
 
     </div>
