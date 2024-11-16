3d3d2f9f522b5820e87250f4b0f468e9d9013112
rbtavares <73722899+rbtavares@users.noreply.github.com>
2024-11-16 19:44:17 +0000

    tweaked dialogs

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -6,18 +6,28 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { Label } from "./ui/label"
 import { Settings } from "lucide-react"
+import { useBackground } from "@/hooks/useBackground"
+import { BackgroundProvider } from "@/hooks/useBackground"
 
 
 const SettingsDialog = () => {
+
+    const { updateBackground } = useBackground();
+
     return (
         <Dialog>
-            <DialogTrigger><Settings className="size-6 text-white/50 hover:text-white/80 duration-300 hover:animate-spin"/></DialogTrigger>
+            <DialogTrigger><Settings className="size-6 text-white/50 hover:text-white/80 duration-300 hover:animate-spin" /></DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Settings</DialogTitle>
+                    <DialogTitle className="mb-5">Settings</DialogTitle>
                     <DialogDescription>
-                        Nothing Here
+                        <Label>Background</Label>
+                        <div className="grid grid-cols-2 gap-2">
+                            <div className="aspect-video bg-shapes w-full bg-cover rounded  cursor-pointer" onClick={() => updateBackground('bg-shapes')} />
+                            <div className="aspect-video bg-shapes2 w-full bg-cover rounded  cursor-pointer" onClick={() => updateBackground('bg-shapes2')} />
+                        </div>
                     </DialogDescription>
                 </DialogHeader>
             </DialogContent>
