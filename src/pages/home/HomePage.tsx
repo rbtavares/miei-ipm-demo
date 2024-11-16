c2d1b3577daab8361c026c936d68087868a1694d
rbtavares <73722899+rbtavares@users.noreply.github.com>
2024-11-16 21:16:48 +0000

    fixed shit

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,11 +1,12 @@
 import NotificationsCard from "@/cards/NotificationsCard";
+import UpcomingEventsCard from "@/cards/UpcomingEventsCard";
 import Navbar from "@/components/Navbar";
 import { useBackground } from "@/hooks/useBackground";
 import { getPath } from "@/lib/utils";
 import Courses from "@/pages/home/Courses";
 import ProfileSummary from "@/pages/home/ProfileSummary";
 import Progress from "@/pages/home/Progress";
-import ScheduleSummary from "./ScheduleSummary";
+import Calendar from "./Calendar";
 
 const breadcrumbRoutes = [
   { name: 'Home', dest: getPath('/home') },
@@ -17,7 +18,7 @@ const HomePage = () => {
   return (
     <div className={`min-h-screen w-full flex flex-col items-center bg-[#010D10] ${backgroundClass} bg-center bg-cover bg-fixed`}>
 
-      <div className="max-w-screen-2xl flex flex-col gap-2 2xl:gap-5 w-full flex-1 pt-0 p-2 2xl:pt-0 2xl:p-5">
+      <div className="max-w-screen-2xl h-full overflow-hidden flex flex-col gap-2 2xl:gap-5 w-full flex-1 pt-0 p-2 2xl:pt-0 2xl:p-5">
 
         {/* Navbar */}
         <Navbar routes={breadcrumbRoutes} />
@@ -27,18 +28,25 @@ const HomePage = () => {
 
           {/* Left Pane */}
           <div className="flex flex-col gap-2 2xl:gap-5">
-            <ProfileSummary />
-            <Progress progress={120} />
+            <ProfileSummary/>
+            <Progress progress={120}/>
           </div>
 
           {/* Center Pane */}
           <div className="flex flex-col h-full gap-2 2xl:gap-5 col-span-2">
-              <ScheduleSummary />
-              <Courses />
+            <div className="flex flex-1 gap-5 max-h-96">
+              <div className="relative h-full min-w-64">
+                <div className="absolute w-full h-full">
+                  <UpcomingEventsCard />
+                </div>
+              </div>
+              <Calendar />
+            </div>
+            <Courses  />
           </div>
 
           {/* Right Pane */}
-          <NotificationsCard />
+          <NotificationsCard  />
 
         </div>
 
